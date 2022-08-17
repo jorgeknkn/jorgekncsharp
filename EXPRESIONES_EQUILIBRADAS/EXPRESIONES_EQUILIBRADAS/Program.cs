640a246a5dc7305076390eb694dc3d47cb50c783
CMBOTI01\cmbouser <guillermo.sanchez@bochetti.com.mx>
2022-08-17 12:48:59 -0500

    Conjunto de mi tests

---
+++ EXPRESIONES_EQUILIBRADAS/EXPRESIONES_EQUILIBRADAS/Program.cs
@@ -0,0 +1,77 @@
+﻿/*
+ * Reto #10
+ * EXPRESIONES EQUILIBRADAS
+ * Fecha publicación enunciado: 07/03/22
+ * Fecha publicación resolución: 14/03/22
+ * Dificultad: MEDIA
+ *
+ * Enunciado: Crea un programa que comprueba si los paréntesis, llaves y corchetes de una expresión están equilibrados.
+ * - Equilibrado significa que estos delimitadores se abren y cieran en orden y de forma correcta.
+ * - Paréntesis, llaves y corchetes son igual de prioritarios. No hay uno más importante que otro.
+ * - Expresión balanceada: { [ a * ( c + d ) ] - 5 }
+ * - Expresión no balanceada: { a * ( c + d ) ] - 5 }
+ */
+
+using System;
+using System.Collections.Generic;
+
+namespace EXPRESIONES_EQUILIBRADAS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(IsBalanced("{a + b [c] * (2x2)}}}}"));
+            Console.WriteLine(IsBalanced("{ [ a * ( c + d ) ] - 5 }"));
+            Console.WriteLine(IsBalanced("{ a * ( c + d ) ] - 5 }"));
+            Console.WriteLine(IsBalanced("{a^4 + (((ax4)}"));
+            Console.WriteLine(IsBalanced("{ ] a * ( c + d ) + ( 2 - 3 )[ - 5 }"));
+            Console.WriteLine(IsBalanced("{{{{{{(}}}}}}"));
+            Console.WriteLine(IsBalanced("(a"));
+            Console.ReadKey();
+        }
+
+        private static bool IsBalanced(string expression)
+        {
+            Stack<char> st = new Stack<char>();
+
+            for (int i = 0; i < expression.Length; i++)
+            {
+                if (expression[i] == '{' || expression[i] == '(' || expression[i] == '[')
+                    st.Push(expression[i]);
+
+                if (expression[i] == '}' || expression[i] == ')' || expression[i] == ']')
+                {
+                    if (st.Count == 0)
+                    {
+                        return false;
+                    }
+                    else if (!IsMatchingPair(st.Pop(), expression[i]))
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            if (st.Count == 0)
+                return true; // balanced
+            else
+            {
+                // not balanced
+                return false;
+            }
+        }
+
+        static bool IsMatchingPair(char character1, char character2)
+        {
+            if (character1 == '(' && character2 == ')')
+                return true;
+            else if (character1 == '{' && character2 == '}')
+                return true;
+            else if (character1 == '[' && character2 == ']')
+                return true;
+            else
+                return false;
+        }
+    }
+}
---
+++ FIZZ BUZZ/FIZZ BUZZ/Program.cs
@@ -0,0 +1,54 @@
+﻿/*
+ * Reto #0
+ * EL FAMOSO "FIZZ BUZZ"
+ * Fecha publicación enunciado: 27/12/21
+ * Fecha publicación resolución: 03/01/22
+ * Dificultad: FÁCIL
+ * Enunciado: Escribe un programa que muestre por consola (con un print) los números de 1 a 100 (ambos incluidos y con un salto de línea entre cada impresión), sustituyendo los siguientes:
+ * - Múltiplos de 3 por la palabra "fizz".
+ * - Múltiplos de 5 por la palabra "buzz".
+ * - Múltiplos de 3 y de 5 a la vez por la palabra "fizzbuzz".
+ */
+using System;
+using System.Linq;
+
+namespace FIZZ_BUZZ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] collection = new int[100];
+            collection = FillArrayInt();
+
+            foreach (var item in collection)
+            {
+                var divisibleByThree = item % 3 == 0;
+                var divisibleByFive = item % 5 == 0;
+
+                if (divisibleByThree && divisibleByFive)
+                {
+                    Console.WriteLine("fizzbuzz!");
+                }
+                else if (divisibleByThree)
+                {
+                    Console.WriteLine("fizz!");
+                }
+                else if (divisibleByFive)
+                {
+                    Console.WriteLine("buzz!");
+                }
+                else
+                {
+                    Console.WriteLine(item.ToString());
+                }
+            }
+            Console.ReadKey();
+        }
+
+        private static int[] FillArrayInt()
+        {
+            return Enumerable.Range(1, 100).ToArray();
+        }
+    }
+}
